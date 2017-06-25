51e8657b76ab0577175e38f4c2325f8bbd480e9d
mcgridles <henry.gridley@gmail.com>
2017-06-25 16:02:22 -0400

    wrote base template and created teams file
    created teams view and added url
    
    
    created teams app
    
    
    added teams app
    
    
    cleaned up templates, added app_name
    
    
    cleaned up file
    
    
    added app_name
    
    
    removed hardcoded links
    
    
    changed dir names

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'accounts'
 urlpatterns = [
     url(r'^login/$', views.login, name='login'),
     url(r'^signup/$', views.signup, name='signup'),
--- teams/urls.py
+++ teams/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = 'home'
+app_name = 'teams'
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^$', views.teams, name='dashboard'),
 ]
