3bfb21fe79c79cf6ab7636305ac9da3906494b35
mcgridles <henry.gridley@gmail.com>
2017-06-29 00:02:24 -0400

    first draft
    Primarily focused on Team and Swimmer structure, Practices and schedule needs more work.

--- teams/models.py
+++ teams/models.py
@@ -3,4 +3,113 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+# Not sure how to associate things with a user but we may need to give the
+# Team and Week classes a ForeignKey field for a user
+
+
+# Swimmers & Teams
+
+class Team(models.Model):
+    name = models.CharField(max_length=50)
+    num_swimmers = models.IntegerField()
+    region = models.CharField(max_length=25)
+
+class Swimmer(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+    f_name = models.CharField(max_length=25)
+    l_name = models.CharField(max_length=25)
+    gender = models.CharField(max_length=10)
+    age = models.IntegerField()
+    bio = models.TextField()
+
+
+# Workout
+
+class Rep(models.Model):
+    set_id = models.ForeignKey(Set, on_delete=models.CASCADE)
+
+    num = models.IntegerField()
+    distance = models.IntegerField()
+    stroke = models.CharField(max_length=10)
+    comments = models.CharField(max_length=100) # switch to TextField?
+
+class Set(models.Model):
+    practice = models.ForeignKey(Practice, on_delete=models.CASCADE)
+    repeats = models.IntegerField()
+
+class Practice(models.Model):
+    week = models.ForeignKey(Week, on_delete=models.CASCADE)
+    weekday = models.CharField(max_length=10)
+    description = models.TextField()
+
+
+# Calendar
+
+# Need to be able to associate practice with a day of the week
+# Could add more class to organize weeks like a calendar or could do it in views
+class Week(models.Model):
+    # start and end can be used to keep track of dates
+    start = models.DateField()
+    end = models.DateField()
+
+
+# Events
+
+# Many-to-one -> many times can be created and associated with one swimmer
+# Makes it easy to sort and query for fastest time
+class Free_50(models.Model):
+    swimmer = models.ForeignKey(Swimmer, on_delete=models.CASCADE)
+    time = models.DurationField()
+
+class Free_100(models.Model):
+    swimmer = models.ForeignKey(Swimmer, on_delete=models.CASCADE)
+    time = models.DurationField()
+
+class Free_200(models.Model):
+    swimmer = models.ForeignKey(Swimmer, on_delete=models.CASCADE)
+    time = models.DurationField()
+
+class Free_500(models.Model):
+    swimmer = models.ForeignKey(Swimmer, on_delete=models.CASCADE)
+    time = models.DurationField()
+
+class Free_1000(models.Model):
+    swimmer = models.ForeignKey(Swimmer, on_delete=models.CASCADE)
+    time = models.DurationField()
+
+class Fly_50(models.Model):
+    swimmer = models.ForeignKey(Swimmer, on_delete=models.CASCADE)
+    time = models.DurationField()
+
+class Fly_100(models.Model):
+    swimmer = models.ForeignKey(Swimmer, on_delete=models.CASCADE)
+    time = models.DurationField()
+
+class Fly_200(models.Model):
+    swimmer = models.ForeignKey(Swimmer, on_delete=models.CASCADE)
+    time = models.DurationField()
+
+class Back_50(models.Model):
+    swimmer = models.ForeignKey(Swimmer, on_delete=models.CASCADE)
+    time = models.DurationField()
+
+class Back_100(models.Model):
+    swimmer = models.ForeignKey(Swimmer, on_delete=models.CASCADE)
+    time = models.DurationField()
+
+class Back_200(models.Model):
+    swimmer = models.ForeignKey(Swimmer, on_delete=models.CASCADE)
+    time = models.DurationField()
+
+class Breast_50(models.Model):
+    swimmer = models.ForeignKey(Swimmer, on_delete=models.CASCADE)
+    time = models.DurationField()
+
+class Breast_100(models.Model):
+    swimmer = models.ForeignKey(Swimmer, on_delete=models.CASCADE)
+    time = models.DurationField()
+
+class Breast_200(models.Model):
+    swimmer = models.ForeignKey(Swimmer, on_delete=models.CASCADE)
+    time = models.DurationField()
