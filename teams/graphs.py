c62f268a682ae665dcfc2fd40f6479e3548c68d4
mcgridles <henry.gridley@gmail.com>
2017-08-30 14:55:11 -0700

    refresh not working

--- teams/graphs.py
+++ teams/graphs.py
@@ -1,3 +1,5 @@
+import json
+
 from bokeh.plotting import figure, output_file, show
 from bokeh.layouts import column, widgetbox
 from bokeh.embed import components
@@ -8,8 +10,20 @@ from bokeh.models.widgets import Select
 
 from teams.models import Event, EVENT_CHOICE, Swimmer
 
+class DatetimeEncoder(json.JSONEncoder):
+    """
+    Encodes Python datetime.date objects to make compatible with JSON serialization.
+    """
+    def default(self, obj):
+        try:
+            return super(DatetimeEncoder, obj).default(obj)
+        except TypeError:
+            return str(obj)
+
 def date_time_hover_tool():
-    """Generates the HTML for the Bokeh's hover data tool on our graph."""
+    """
+    Generates the HTML for the Bokeh's hover data tool on our graph.
+    """
     hover_html = """
       <div>
         <span class="hover-tooltip">@date</span>
@@ -33,135 +47,188 @@ def graph_event(swimmer):
         tools=tools,
         responsive=True,
     )
+    # format datetime.timedelta objects to MM:ss.mm
+    plot.yaxis.formatter = FuncTickFormatter(code=
+        """
+        return Math.floor(tick/60) + ":" + tick.toFixed(2)
+        """
+    )
 
-    plot_features = {}
+    data_source = {}
     events = []
-    first_event = True
-
+    first_event = None
     for event in EVENT_CHOICE:
-        data_source = {}
-        e = '_'.join(event[0].split())
+        e = '_'.join([word.lower() for word in event[0].split()])
 
         results = Event.objects.filter(swimmer=swimmer).filter(event=event[0]).order_by('date')
         if results.exists():
-            if len(results) == 1:
-                plot_features['line_'+e] = None
+            if len(results) == 1: # one point will not display well on graph
+                data_source['x_'+e] = None
+                data_source['y_'+e] = None
+                data_source['date_'+e] = None
+                data_source['time_'+e] = None
                 continue
 
             events.append(event[1])
-            x = []
-            y = []
-            date = []
-            time = []
-            for r in results:
+            if first_event == None:
+                first_event = e
+
+            x, y = [], []
+            date, time = [], []
+            for r in results.iterator():
                 d = r.date
                 t = r.time.total_seconds()
                 x.append(d)
                 y.append(t)
-                date.append(d.strftime('%m/%d/%y'))
-                time.append('{:d}:{:.2f}'.format(int(t)/60, t))
+                date.append(d.strftime('%m/%d/%y')) # date to string for hover
+                time.append('{:d}:{:.2f}'.format(int(t)/60, t)) # time to string for hover
 
             data_source['x_'+e] = x
             data_source['y_'+e] = y
-            data_source['date'] = date
-            data_source['time'] = time
-
-            source = ColumnDataSource(data_source)
-
-            plot_features['line_'+e] = plot.line('x_'+e, 'y_'+e, line_width=2, source=source)
-
-            if first_event == True:
-                plot_features['line_'+e].visible = True
-                first_event = False
-            else:
-                plot_features['line_'+e].visible = False
-
-            plot.yaxis.formatter = FuncTickFormatter(code=
-                """
-                return Math.floor(tick/60) + ":" + tick.toFixed(2)
-                """
-            )
+            data_source['date_'+e] = date
+            data_source['time_'+e] = time
 
         else:
-            plot_features['line_'+e] = None
+            # eliminates KeyError exceptions
+            data_source['x_'+e] = None
+            data_source['y_'+e] = None
+            data_source['date_'+e] = None
+            data_source['time_'+e] = None
 
-    # callback shows and hides lines depending on Select box
-    callback = CustomJS(args=dict(
-            line_50_free=plot_features['line_50_free'],
-            line_100_free=plot_features['line_100_free'],
-            line_200_free=plot_features['line_200_free'],
-            line_500_free=plot_features['line_500_free'],
-            line_1000_free=plot_features['line_1000_free'],
-            line_50_back=plot_features['line_50_back'],
-            line_100_back=plot_features['line_100_back'],
-            line_200_back=plot_features['line_200_back'],
-            line_50_breast=plot_features['line_50_breast'],
-            line_100_breast=plot_features['line_100_breast'],
-            line_200_breast=plot_features['line_200_breast'],
-            line_50_fly=plot_features['line_50_fly'],
-            line_100_fly=plot_features['line_100_fly'],
-            line_200_fly=plot_features['line_200_fly'],
-            line_100_im=plot_features['line_100_im'],
-            line_200_im=plot_features['line_200_im'],
-            line_400_im=plot_features['line_400_im'],
-            line_base_free=plot_features['line_base_free'],
-            line_base_back=plot_features['line_base_back'],
-            line_base_breast=plot_features['line_base_breast'],
-            line_base_fly=plot_features['line_base_fly'],
-            line_base_im=plot_features['line_base_IM'],
-        ), code="""
-            f = cb_obj.value;
-
-            if (line_50_free) { line_50_free.visible = false; }
-            if (line_100_free) { line_100_free.visible = false; }
-            if (line_200_free)  { line_200_free.visible = false; }
-            if (line_500_free)  { line_500_free.visible = false; }
-            if (line_1000_free)  { line_1000_free.visible = false; }
-            if (line_50_back)  { line_50_back.visible = false; }
-            if (line_100_back)  { line_100_back.visible = false; }
-            if (line_200_back)  { line_200_back.visible = false; }
-            if (line_50_breast)  { line_50_breast.visible = false; }
-            if (line_100_breast)  { line_100_breast.visible = false; }
-            if (line_200_breast)  { line_200_breast.visible = false; }
-            if (line_50_fly)  { line_50_fly.visible = false; }
-            if (line_100_fly)  { line_100_fly.visible = false; }
-            if (line_200_fly)  { line_200_fly.visible = false; }
-            if (line_100_im)  { line_100_im.visible = false; }
-            if (line_200_im)  { line_200_im.visible = false; }
-            if (line_400_im)  { line_400_im.visible = false; }
-            if (line_base_free)  { line_base_free.visible = false; }
-            if (line_base_back)  { line_base_back.visible = false; }
-            if (line_base_breast)  { line_base_breast.visible = false; }
-            if (line_base_fly)  { line_base_fly.visible = false; }
-            if (line_base_im)  { line_base_im.visible = false; }
-
-            if (f == "50 Freestyle") { line_50_free.visible = true; }
-            else if (f == "100 Freestyle") { line_100_free.visible = true; }
-            else if (f == "200 Freestyle")  { line_200_free.visible = true; }
-            else if (f == "500 Freestyle")  { line_500_free.visible = true; }
-            else if (f == "1000 Freestyle")  { line_1000_free.visible = true; }
-            else if (f == "50 Backstroke")  { line_50_back.visible = true; }
-            else if (f == "100 Backstroke")  { line_100_back.visible = true; }
-            else if (f == "200 Backstroke")  { line_200_back.visible = true; }
-            else if (f == "50 Breaststroke")  { line_50_breast.visible = true; }
-            else if (f == "100 Breaststroke")  { line_100_breast.visible = true; }
-            else if (f == "200 Breaststroke")  { line_200_breast.visible = true; }
-            else if (f == "50 Butterfly")  { line_50_fly.visible = true; }
-            else if (f == "100 Butterfly")  { line_100_fly.visible = true; }
-            else if (f == "200 Butterfly")  { line_200_fly.visible = true; }
-            else if (f == "100 IM")  { line_100_im.visible = true; }
-            else if (f == "200 IM")  { line_200_im.visible = true; }
-            else if (f == "400 IM")  { line_400_im.visible = true; }
-            else if (f == "Base Freestyle")  { line_base_free.visible = true; }
-            else if (f == "Base Backstroke")  { line_base_back.visible = true; }
-            else if (f == "Base Breaststroke")  { line_base_breast.visible = true; }
-            else if (f == "Base Butterfly")  { line_base_fly.visible = true; }
-            else if (f == "Base IM")  { line_base_im.visible = true; }
-    """)
+    # set initial graph
+    source = ColumnDataSource(data=dict(
+        x=data_source['x_'+first_event],
+        y=data_source['y_'+first_event],
+        date=data_source['date_'+first_event],
+        time=data_source['time_'+first_event]
+    ))
+    plot.line('x', 'y', source=source)
 
     try:
-        multi_select = Select(title="Select Event:", value=events[0], options=events, callback=callback)
-        multi_select = widgetbox(multi_select, responsive=True)
-        return components(column(multi_select, plot, responsive=True))
+        select = Select(title="Select Event:", value=events[0], options=events)
     except IndexError:
         return None, None
+
+    # callback modifies data source depending on Select box
+    callback = CustomJS(args=dict(source=source, select=select), code="""
+            data = %s;
+
+            if (select.value == "50 Freestyle") {
+                source.data['x'] = data.x_50_free;
+                source.data['y'] = data.y_50_free;
+                source.data['date'] = data.date_50_free;
+                source.data['time'] = data.time_50_free;
+            } else if (select.value == "100 Freestyle") {
+                source.data['x'] = data.x_100_free;
+                source.data['y'] = data.y_100_free;
+                source.data['date'] = data.date_100_free;
+                source.data['time'] = data.time_100_free;
+            } else if (select.value == "200 Freestyle") {
+                console.log(select.value);
+                source.data['x'] = data.x_200_free;
+                source.data['y'] = data.y_200_free;
+                source.data['date'] = data.date_200_free;
+                source.data['time'] = data.time_200_free;
+            } else if (select.value == "500 Freestyle") {
+                source.data['x'] = data.x_500_free;
+                source.data['y'] = data.y_500_free;
+                source.data['date'] = data.date_500_free;
+                source.data['time'] = data.time_500_free;
+            } else if (select.value == "1000 Freestyle") {
+                source.data['x'] = data.x_1000_free;
+                source.data['y'] = data.y_1000_free;
+                source.data['date'] = data.date_1000_free;
+                source.data['time'] = data.time_1000_free;
+            } else if (select.value == "50 Backstroke") {
+                source.data['x'] = data.x_50_back;
+                source.data['y'] = data.y_50_back;
+                source.data['date'] = data.date_50_back;
+                source.data['time'] = data.time_50_back;
+            } else if (select.value == "100 Backstroke") {
+                source.data['x'] = data.x_100_back;
+                source.data['y'] = data.y_100_back;
+                source.data['date'] = data.date_100_back;
+                source.data['time'] = data.time_100_back;
+            } else if (select.value == "200 Backstroke") {
+                source.data['x'] = data.x_200_back;
+                source.data['y'] = data.y_200_back;
+                source.data['date'] = data.date_200_back;
+                source.data['time'] = data.time_200_back;
+            } else if (select.value == "50 Breaststroke") {
+                source.data['x'] = data.x_50_breast;
+                source.data['y'] = data.y_50_breast;
+                source.data['date'] = data.date_50_breast;
+                source.data['time'] = data.time_50_breast;
+            } else if (select.value == "100 Breaststroke") {
+                source.data['x'] = data.x_100_breast;
+                source.data['y'] = data.y_100_breast;
+                source.data['date'] = data.date_100_breast;
+                source.data['time'] = data.time_100_breast;
+            } else if (select.value == "200 Breaststroke") {
+                source.data['x'] = data.x_200_breast;
+                source.data['y'] = data.y_200_breast;
+                source.data['date'] = data.date_200_breast;
+                source.data['time'] = data.time_200_breast;
+            } else if (select.value == "50 Butterfly") {
+                source.data['x'] = data.x_50_fly;
+                source.data['y'] = data.y_50_fly;
+                source.data['date'] = data.date_50_fly;
+                source.data['time'] = data.time_50_fly;
+            } else if (select.value == "100 Butterfly") {
+                source.data['x'] = data.x_100_fly;
+                source.data['y'] = data.y_100_fly;
+                source.data['date'] = data.date_100_fly;
+                source.data['time'] = data.time_100_fly;
+            } else if (select.value == "200 Butterfly") {
+                source.data['x'] = data.x_200_fly;
+                source.data['y'] = data.y_200_fly;
+                source.data['date'] = data.date_200_fly;
+                source.data['time'] = data.time_200_fly;
+            } else if (select.value == "100 IM") {
+                source.data['x'] = data.x_100_im;
+                source.data['y'] = data.y_100_im;
+                source.data['date'] = data.date_100_im;
+                source.data['time'] = data.time_100_im;
+            } else if (select.value == "200 IM") {
+                source.data['x'] = data.x_200_im;
+                source.data['y'] = data.y_200_im;
+                source.data['date'] = data.date_200_im;
+                source.data['time'] = data.time_200_im;
+            } else if (select.value == "400 IM") {
+                source.data['x'] = data.x_400_im;
+                source.data['y'] = data.y_400_im;
+                source.data['date'] = data.date_400_im;
+                source.data['time'] = data.time_400_im;
+            } else if (select.value == "Base Freestyle") {
+                source.data['x'] = data.x_base_free;
+                source.data['y'] = data.y_base_free;
+                source.data['date'] = data.date_base_free;
+                source.data['time'] = data.time_base_free;
+            } else if (select.value == "Base Backstroke") {
+                source.data['x'] = data.x_base_back;
+                source.data['y'] = data.y_base_back;
+                source.data['date'] = data.date_base_back;
+                source.data['time'] = data.time_base_back;
+            } else if (select.value == "Base Breaststroke") {
+                source.data['x'] = data.x_base_breast;
+                source.data['y'] = data.y_base_breast;
+                source.data['date'] = data.date_base_breast;
+                source.data['time'] = data.time_base_breast;
+            } else if (select.value == "Base Butterfly") {
+                source.data['x'] = data.x_base_fly;
+                source.data['y'] = data.y_base_fly;
+                source.data['date'] = data.date_base_fly;
+                source.data['time'] = data.time_base_fly;
+            } else if (select.value == "Base IM") {
+                source.data['x'] = data.x_base_im;
+                source.data['y'] = data.y_base_im;
+                source.data['date'] = data.date_base_im;
+                source.data['time'] = data.time_base_im;
+            }
+
+            source.change.emit()
+    """ % json.dumps(data_source, cls=DatetimeEncoder))
+
+    select.callback = callback
+
+    return components(column(select, plot, responsive=True))
