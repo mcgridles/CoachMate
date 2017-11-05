d6c73c4d5150e32d5d2a03ed259bb1b6d47139bf
Henry Gridley <henry.gridley@gmail.com>
2017-11-05 11:00:28 -0500

    added link to account settings page
    fixed age-based test
    
    
    added ability to change password

--- accounts/views.py
+++ accounts/views.py
@@ -4,8 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render, redirect
 from django.contrib import auth
 from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
 
-from accounts.forms import SignUpForm, LogInForm
+from accounts.forms import SignUpForm, LogInForm, SettingsForm
 
 @csrf_protect
 def login(request):
@@ -45,3 +48,33 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'accounts/signup.html', {'form': form})
+
+@csrf_protect
+@login_required
+def settings(request):
+    if request.method == 'POST':
+        form = SettingsForm(request.POST)
+        if form.is_valid():
+            old_passwd = form.cleaned_data['old_passwd']
+            new_passwd1 = form.cleaned_data['new_passwd1']
+            new_passwd2 = form.cleaned_data['new_passwd2']
+
+            user = auth.authenticate(username=request.user.username, password=old_passwd)
+            if user is not None:
+                if new_passwd1 == new_passwd2:
+                    user.set_password(new_passwd1)
+                    user.save()
+                    auth.login(request, user) # log in user
+                    messages.success(request, 'Password changed')
+                else:
+                    messages.error(request, 'Passwords do not match')
+            else:
+                messages.error(request, 'Incorrect password')
+        else:
+            messages.error(request, 'Invalid entry')
+
+        return redirect('accounts:settings')
+
+    else:
+        form = SettingsForm()
+    return render(request, 'accounts/settings.html', {'form': form, 'user': request.user})
